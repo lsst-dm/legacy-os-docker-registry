5be728c04630c0b32963586198b03f4157acc469
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-03-14 22:31:38 +0100

    Fix proxy

---
+++ bare-metal/README.md
@@ -0,0 +1,1 @@
+TODO: Add NO_PROXY=ccqservreg.in2p3.fr to all node in /etc/sysconfig/docker
--- bare-metal/run.sh
+++ bare-metal/run.sh
@@ -2,8 +2,12 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 REGISTRY_FILE="$DIR/registry-config.yml"
 
+NON_AFS_REGISTRY_FILE="/tmp/registry-config.yml"
+
+cp "$REGISTRY_FILE" "$NON_AFS_REGISTRY_FILE"
+
 docker run -d --restart=always -p 5000:5000 --name registry-mirror \
-    -v /etc/docker/registry-config.yml:$REGISTRY_FILE -e STANDALONE=false \
+    -v $NON_AFS_REGISTRY_FILE:/etc/docker/registry/config.yml -e STANDALONE=false \
     -e MIRROR_SOURCE=https://registry-1.docker.io \
     -e MIRROR_SOURCE_INDEX=https://index.docker.io \
     registry:2
